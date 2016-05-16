32ff5668a43784b1076a1da1f8098c6fa2cf6a60
MRixen <ManLeo.Rixen@gmail.com>
2016-05-16 17:45:39 +0200

    Now its possible to send sensor data after a request from raspberry

--- mcpExecutor.ino
+++ mcpExecutor.ino
@@ -1,9 +1,10 @@
 #include <SPI.h>
 
+// TODO Print only in debug mode -> Add input to enable debuging
+
 const int CS_PIN_ADXL = 10;
 const int CS_PIN_MCP2515 = 9;
-const int START_PIN_IN = 7;
-const int END_PIN_OUT = 8;
+const int MCP2515_PIN_INTE_SENDER = 8;
 
 // CONTROL REGISTER
 const byte CONTROL_REGISTER_BFPCTRL = 0x0C;
@@ -37,14 +38,23 @@ const byte REGISTER_TXB0D5 = 0x3B;
 const byte REGISTER_TXB0D6 = 0x3C;
 const byte REGISTER_TXB0D7 = 0x3D;
 const byte REGISTER_TXB0Dx[] = { REGISTER_TXB0D0, REGISTER_TXB0D1, REGISTER_TXB0D2, REGISTER_TXB0D3, REGISTER_TXB0D4, REGISTER_TXB0D5, REGISTER_TXB0D6, REGISTER_TXB0D7};
-const byte REGISTER_TXB0SIDL_VALUE = 0x00;
-const byte REGISTER_TXB0SIDH_VALUE = 0x01; // Identifier for the mcpExecutor
+const byte REGISTER_RXB0D0 = 0x66;
+const byte REGISTER_RXB0D1 = 0x67;
+const byte REGISTER_RXB0D2 = 0x68;
+const byte REGISTER_RXB0D3 = 0x69;
+const byte REGISTER_RXB0D4 = 0x6A;
+const byte REGISTER_RXB0D5 = 0x6B;
+const byte REGISTER_RXB0D6 = 0x6C;
+const byte REGISTER_RXB0D7 = 0x6D;
+const byte REGISTER_RXB0Dx[] = { REGISTER_RXB0D0, REGISTER_RXB0D1, REGISTER_RXB0D2, REGISTER_RXB0D3, REGISTER_RXB0D4, REGISTER_RXB0D5, REGISTER_RXB0D6, REGISTER_RXB0D7 };
+const byte REGISTER_TXB0SIDL_VALUE = 0x01;
+const byte REGISTER_TXB0SIDH_VALUE = 0x00; // Identifier for the mcpExecutor
 const byte CONTROL_REGISTER_CANSTAT_NORMAL_MODE = 0x00;
 const byte CONTROL_REGISTER_CANSTAT_SLEEP_MODE = 0x20;
 const byte CONTROL_REGISTER_CANSTAT_LOOPBACK_MODE = 0x40;
 const byte CONTROL_REGISTER_CANSTAT_LISTEN_ONLY_MODE = 0x60;
 const byte CONTROL_REGISTER_CANSTAT_CONFIGURATION_MODE = 0x80;
-const byte CONTROL_REGISTER_CANINTE_INTE = 0x00;
+const byte CONTROL_REGISTER_CANINTE_INTE = 0x01;
 const byte CONTROL_REGISTER_TXB0CTRL_VALUE = 0x00;
 
 // Set values for bit timing
@@ -60,6 +70,17 @@ const byte CONTROL_REGISTER_CANCTRL_SLEEP_MODE = 0x20;
 const byte CONTROL_REGISTER_CANCTRL_LOOPBACK_MODE = 0x40;
 const byte CONTROL_REGISTER_CANCTRL_LISTEN_ONLY_MODE = 0x60;
 const byte CONTROL_REGISTER_CANCTRL_CONFIGURATION_MODE = 0x80;
+
+// Set values for interrupt flags mcp2515
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_ALL_IF = 0x00;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_MERRIF = 0x7F;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_WAKIF = 0xBF;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_ERRIF = 0xDF;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_TX2IF = 0xEF;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_TX1IF = 0xF7;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_TX0IF = 0xFB;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_RX1IF = 0xFD;
+const byte CONTROL_REGISTER_CANINTF_VALUE_RESET_RX0IF = 0xFE;
 
 // SPI INSTRUCTIONS MCP2515
 const byte SPI_INSTRUCTION_RESET = 0xC0;
@@ -87,38 +108,67 @@ const byte ACCEL_SPI_RW_BIT = 0x80; /* Bit used in SPI transactions to indicate 
 const byte ACCEL_SPI_MB_BIT = 0x40; /* Bit used to indicate multi-byte SPI transactions     */
 const byte MESSAGE_SIZE_ADXL = 0x06;
 
+// DATA FOR ADXL
+const int ACCEL_RES = 1024;         /* The ADXL345 has 10 bit resolution giving 1024 unique values                     */
+const int ACCEL_DYN_RANGE_G = 8;    /* The ADXL345 had a total dynamic range of 8G, since we're configuring it to +-4G */
+const int UNITS_PER_G = ACCEL_RES / ACCEL_DYN_RANGE_G;  /* Ratio of raw int values to G units                          */
+														// TEST
+union acc_x
+{
+	short accelerationRawX;
+	byte bytes[2];
+};
+union acc_y
+{
+	short accelerationRawY;
+	byte bytes[2];
+};
+union acc_z
+{
+	short accelerationRawZ;
+	byte bytes[2];
+};
+
+struct Acceleration
+{
+	double accelX;
+	double accelY;
+	double accelZ;
+};
+
+double sensorData[3];
+
 byte ReadBuf[6]; // Read buffer of size 6 bytes (2 bytes * 3 axes)
-byte retVal;
 
 const int ADXL = 1;
 const int MCP2515 = 2;
-const int	NO_DEVICE = 3;
+const int NO_DEVICE = 3;
+
+// TEST
+boolean commandExecuted;
 
 
 void setup()
 {
+	// TEST
+	commandExecuted = false;
+
 	// Define I/Os
 	pinMode(CS_PIN_ADXL, OUTPUT);
 	pinMode(CS_PIN_MCP2515, OUTPUT); // Set as input to enable pull up resistor. It's neccessary because the ss line is defined at pin 10 + 9
-	pinMode(START_PIN_IN, INPUT);
-	pinMode(END_PIN_OUT, OUTPUT);
+	pinMode(MCP2515_PIN_INTE_SENDER, INPUT); 
 
 	// Configure I/Os
 	digitalWrite(CS_PIN_ADXL, HIGH);
 	digitalWrite(CS_PIN_MCP2515, HIGH);
-	digitalWrite(END_PIN_OUT, LOW);
 
 	// Configure SPI
 	SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE3));
 	SPI.begin();
 
 	// Configure ADXL and MCP2515
-	switchDevice(ADXL);
 	initAdxl();
-	switchDevice(MCP2515);
-	//initMcp2515();
-	delay(100);
-	switchDevice(NO_DEVICE);
+	initMcp2515();
 
 	// Give time to set up
 	delay(100);
@@ -126,22 +176,37 @@ void setup()
 
 void loop()
 {
-	//if (digitalRead(START_PIN_IN) == 1)
-	//{
-		readAccel(); // Read sensor data
-		//sendToMcp2515(); // Send sensor data to mcp2515
-		//digitalWrite(END_PIN_OUT, HIGH);
-		//waitFor(START_PIN_IN, 0); // Wait until master reset the signal -> Handshake
-		//digitalWrite(END_PIN_OUT, LOW);
-	//}
-		//for (int i = 0; i < 6; i++)
-		//{
-	Serial.println("ReadBuffer: " + ReadBuf[0]);
-		//}
-		delay(1000);
+	// Wait for message to start execution
+	if (digitalRead(MCP2515_PIN_INTE_SENDER) == 0)
+	{	
+		byte retVal[2];
+
+		// Init mcp2515 to send a message (define identifier, message length, etc.)
+		mcp2515_init_tx_buffer0(REGISTER_TXB0SIDL_VALUE, REGISTER_TXB0SIDH_VALUE, 0x02);
+
+		// Read message to check if it is the right selector id
+		for (int i = 0; i < 2; i++)
+		{
+			retVal[i] = mcp2515_execute_read_command(REGISTER_RXB0Dx[i], CS_PIN_MCP2515);
+			Serial.print("Message identifier at ");
+			Serial.print(i);
+			Serial.print(" is ");
+			Serial.println(retVal[i]);
+		}
+
+		// Check message
+		if ((retVal[0] == REGISTER_TXB0SIDL_VALUE) && (retVal[1] == REGISTER_TXB0SIDH_VALUE)) {
+
+			Serial.println("Read sensor");
+			acquireAndSend(); // Read sensor data
+			mcp2515_load_tx_buffer0(); // Send sensor data to mcp2515
+		}
+	}
 }
 
 void initAdxl() {
+	switchDevice(ADXL);
+
 	writeToSpi(ACCEL_REG_DATA_FORMAT, 0x01, CS_PIN_ADXL);
 	writeToSpi(ACCEL_REG_POWER_CONTROL, 0x08, CS_PIN_ADXL);
 }
@@ -179,6 +244,9 @@ void mcp2515_execute_reset_command() {
 	{
 		actualMode = mcp2515_execute_read_command(CONTROL_REGISTER_CANSTAT, CS_PIN_MCP2515);
 	}
+
+	Serial.print("Mcp2515 reset succesfully and switch do mode ");
+	Serial.println(actualMode);
 }
 
 void mcp2515_configureCanBus() {
@@ -189,14 +257,20 @@ void mcp2515_configureCanBus() {
 	mcp2515_execute_write_command(CONTROL_REGISTER_CNF2, CONTROL_REGISTER_VALUE_CNF2, CS_PIN_MCP2515);
 
 	mcp2515_execute_write_command(CONTROL_REGISTER_CNF3, CONTROL_REGISTER_VALUE_CNF3, CS_PIN_MCP2515);
+
+	Serial.println("Mcp2515 configure bus succesfully");
 }
 
 void mcp2515_configureInterrupts() {
 	mcp2515_execute_write_command(CONTROL_REGISTER_CANINTE, CONTROL_REGISTER_CANINTE_INTE, CS_PIN_MCP2515);
+
+	Serial.println("Mcp2515 configure unterrupts succesfully");
 }
 
 void mcp2515_configureMasksFilters() {
 	mcp2515_execute_write_command(CONTROL_REGISTER_TXB0CTRL, CONTROL_REGISTER_TXB0CTRL_VALUE, CS_PIN_MCP2515);
+
+	Serial.println("Mcp2515 configure masks / filters succesfully");
 }
 
 void mcp2515_switchMode(byte modeToCheck, byte modeToSwitch){
@@ -211,6 +285,10 @@ void mcp2515_switchMode(byte modeToCheck, byte modeToSwitch){
 	{
 		actualMode = mcp2515_execute_read_command(CONTROL_REGISTER_CANSTAT, CS_PIN_MCP2515);
 	}
+
+	Serial.print("Mcp2515 switch to mode ");
+	Serial.print(actualMode);
+	Serial.println(" succesfully");
 }
 
 boolean waitFor(int cs_pin, int state) {
@@ -231,18 +309,23 @@ byte mcp2515_execute_read_command(byte registerToRead, int cs_pin)
 	SPI.transfer(SPI_INSTRUCTION_READ);
 
 	// Write the address of the register to read
-	returnMessage = SPI.transfer(registerToRead);
+	SPI.transfer(registerToRead);
+	returnMessage = SPI.transfer(0x00);
 
 	// Disable device
 	digitalWrite(cs_pin, HIGH);
 
+	delay(10);
+
+	mcp2515_execute_write_command(CONTROL_REGISTER_CANINTF, CONTROL_REGISTER_CANINTF_VALUE_RESET_ALL_IF, CS_PIN_MCP2515);
+
 	return returnMessage;
 }
 
-void readAccel() {
-	byte RegAddrBuf[] = { ACCEL_REG_X | ACCEL_SPI_RW_BIT | ACCEL_SPI_MB_BIT };
-
+void acquireAndSend() {
 	switchDevice(ADXL);
+
+	byte RegAddrBuf[] = { ACCEL_REG_X | ACCEL_SPI_RW_BIT | ACCEL_SPI_MB_BIT };
 
 	digitalWrite(CS_PIN_ADXL, LOW);
 	SPI.transfer(RegAddrBuf[0]);
@@ -252,29 +335,61 @@ void readAccel() {
 		Serial.println(ReadBuf[i]);
 	}
 	digitalWrite(CS_PIN_ADXL, HIGH);
+
+	acc_x sensorDataX;
+	sensorDataX.bytes[0] = ReadBuf[0];
+	sensorDataX.bytes[1] = ReadBuf[1];
+
+	acc_y sensorDataY;
+	sensorDataY.bytes[0] = ReadBuf[2];
+	sensorDataY.bytes[1] = ReadBuf[3];
+
+	acc_z sensorDataZ;
+	sensorDataZ.bytes[0] = ReadBuf[4];
+	sensorDataZ.bytes[1] = ReadBuf[5];
+
+	sensorData[0] = (double)sensorDataX.accelerationRawX / UNITS_PER_G;
+	sensorData[1] = (double)sensorDataY.accelerationRawY / UNITS_PER_G;
+	sensorData[2] = (double)sensorDataZ.accelerationRawZ / UNITS_PER_G;
+
+	Serial.print("accel.X: ");
+	Serial.println(sensorData[0]);
+
+	Serial.print("accel.Y: ");
+	Serial.println(sensorData[1]);
+
+	Serial.print("accel.Z: ");
+	Serial.println(sensorData[2]);
 }
 
-void sendToMcp2515() {
+void mcp2515_load_tx_buffer0() {
 
 	switchDevice(MCP2515);
 
-	// Set the message identifier to 10000000000 and extended identifier bit to 0
-	mcp2515_execute_write_command(REGISTER_TXB0SIDL, REGISTER_TXB0SIDL_VALUE, CS_PIN_MCP2515);
-
-	// Set data length and set rtr bit to zero (no remote request)
-	mcp2515_execute_write_command(REGISTER_TXB0SIDH, REGISTER_TXB0SIDH_VALUE, CS_PIN_MCP2515);
-
-	// Set data length and set rtr bit to zero (no remote request)
-	mcp2515_execute_write_command(REGISTER_TXB0DLC, MESSAGE_SIZE_ADXL, CS_PIN_MCP2515);
+	mcp2515_init_tx_buffer0(0x01, 0x00, MESSAGE_SIZE_ADXL);
 
 	// Set data (message size of sensor -> 6 bytes) to tx buffer 0
-	for (size_t i = 0; i < 6; i++)
+	for (size_t i = 0; i < MESSAGE_SIZE_ADXL; i++)
 	{
 		mcp2515_execute_write_command(REGISTER_TXB0Dx[i], ReadBuf[i], CS_PIN_MCP2515);
 	}
 
 	// Send message
 	mcp2515_execute_rts_command(0);
+}
+
+void mcp2515_init_tx_buffer0(byte identifierLow, byte identifierHigh, byte messageSize) {
+
+	switchDevice(MCP2515);
+
+	// Set the message identifier to 10000000000 and extended identifier bit to 0
+	mcp2515_execute_write_command(REGISTER_TXB0SIDL, identifierLow, CS_PIN_MCP2515);
+
+	// Set data length and set rtr bit to zero (no remote request)
+	mcp2515_execute_write_command(REGISTER_TXB0SIDH, identifierHigh, CS_PIN_MCP2515);
+
+	// Set data length and set rtr bit to zero (no remote request)
+	mcp2515_execute_write_command(REGISTER_TXB0DLC, messageSize, CS_PIN_MCP2515);
 }
 
 void writeToSpi(byte address, byte data, int cs_pin) {
